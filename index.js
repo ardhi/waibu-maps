c0810e6909da13415fb0cc0805721b296ccf1b87
Ardhi Lukianto <ardhi@lukianto.com>
2025-08-23 23:45:14 +0700

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 async function factory (pkgName) {
   const me = this
 
-  return class WaibuMaps extends this.lib.BajoPlugin {
+  return class WaibuMaps extends this.lib.Plugin {
     constructor () {
       super(pkgName, me.app)
       this.alias = 'wmaps'
